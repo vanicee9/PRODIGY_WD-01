6e1ba1f454d09ccaac6914dd66c1c8348944743d
Vani <sheoranvani18@gmail.com>
2023-09-30 10:18:34 +0530

    Intership commit1

---
+++ script.js
@@ -0,0 +1,8 @@
+//little alert when we click on order now button 
+
+const effect =()=>{
+    alert('Welcome to Shopify!');
+}
+document.querySelector('#order').addEventListener('click', effect);
+
+
